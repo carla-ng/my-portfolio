c2d38f5c6f68217250f95a7b8f1a0449adbf384e
carla-ng <carlitashata@gmail.com>
2023-07-17 19:47:01 +0200

    New navbar.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,79 @@
-// NAVBAR
 
+/********************
+ *      NAVBAR      *
+ ********************/
+const navbar = document.getElementById('navbar');
+const navbarItems = document.querySelectorAll('.navbar__menu li');
+const navbarHamburger = document.getElementById('navbar-hamburger');
+const navbarLogo = document.querySelector('.navbar__logo');
+
+
+// Clicking menu element
+function handleMenuItemClick(event) {
+  // remove 'active' class from all menu items
+  navbarItems.forEach((item) => {
+    item.classList.remove('active');
+  });
+
+  // add 'active' class to the parent <li> of the clicked menu item
+  event.target.closest('li').classList.add('active');
+
+  // mobile: close the navbar after clicking on a menu item
+  if ( window.innerWidth <= 991 ) {
+    navbar.classList.remove('open');
+  }
+}
+
+
+// Attach click event listener to each menu item
+navbarItems.forEach((item) => {
+  item.addEventListener('click', handleMenuItemClick);
+});
+
+
+// Mobile: open/close menu
+navbarHamburger.addEventListener('click', function() {
+    if ( navbar.classList.contains('open') ) {
+        navbar.classList.remove('open');
+    } else {
+        navbar.classList.add('open');
+    }
+});
+
+
+// Mobile: close menu when clicking on navbar logo
+navbarLogo.addEventListener('click', function() {
+    if ( window.innerWidth <= 991 ) {
+      navbar.classList.remove('open');
+    }
+
+    // remove 'active' class from all menu items
+    navbarItems.forEach((item) => {
+        item.classList.remove('active');
+    });
+});
+
+
+// Mobile: close menu on window resize
+window.addEventListener('resize', function() {
+    if ( window.innerWidth > 991 ) {
+        navbar.classList.remove('open');
+    }
+});
+
+
+// Check if there is an anchor in the URL when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    const url = window.location.href;
+    const anchorIndex = url.indexOf('#');
+
+    if ( anchorIndex !== -1 ) {
+      const anchor = url.slice(anchorIndex); // Get the anchor part of the URL
+      const targetItem = document.querySelector(`.navbar__menu li a[href="${anchor}"]`);
+      
+      if ( targetItem ) {
+        targetItem.closest('li').classList.add('active');
+      }
+    }
+    
+  });
